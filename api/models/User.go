193d4080dedf9f7b6ff721c85840a7a3d833ba48
gerokkos <grokkos@and.digital>
2020-11-27 17:06:46 +0000

    skeleton setup

---
+++ api/models/User.go
@@ -0,0 +1,41 @@
+package models
+
+import "time"
+
+//Name struct
+type Name struct {
+	First string `json:"first"`
+	Last  string `json:"last"`
+}
+
+//Registered struct
+type Registered struct {
+	Date time.Time `json:"date"`
+}
+
+//Picture struct
+type Picture struct {
+	Medium string `json:"medium"`
+}
+
+//User struct
+type User struct {
+	ID         uint       `json:"id"`
+	Name       Name       `json:"name"`
+	Email      string     `json:"email"`
+	Cell       string     `json:"cell"`
+	Picture    Picture    `json:"picture"`
+	Registered Registered `json:"registered"`
+}
+
+//UserList struct
+type UserList struct {
+	Users []User `json:"results"`
+}
+
+type Filter struct {
+	Limit         int64  `schema:"limit"`
+	Email         string `schema:"email"`
+	StartingAfter int64  `schema:"starting_after"`
+	EndingBefore  int64  `schema:"ending_before"`
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/gerokkos/clerk
+
+go 1.13
+
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/jinzhu/gorm v1.9.16
+	github.com/joho/godotenv v1.3.0
+	gorm.io/gorm v1.20.7
+)
